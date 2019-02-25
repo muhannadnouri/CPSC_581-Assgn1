8944bc3e73e8d2e65e7578411239f4eaf0fa1d5b
Muhannad <muhannad_2006@hotmail.com>
2019-02-24 18:37:36 -0700

    Upload Changes

--- MyApplication/app/src/main/java/com/example/mverw/myapplication/MainActivity.java
+++ MyApplication/app/src/main/java/com/example/mverw/myapplication/MainActivity.java
@@ -1,10 +1,17 @@
 package com.example.mverw.myapplication;
 
 import android.content.Intent;
+import android.graphics.Color;
+import android.media.MediaPlayer;
+import android.os.Handler;
+import android.provider.Settings;
+import android.support.constraint.ConstraintLayout;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 import android.widget.ImageView;
+import android.widget.TextView;
 //import android.app.ActionBar;
 import java.util.List;
 import java.util.ArrayList;
@@ -13,10 +20,13 @@ public class MainActivity extends AppCompatActivity {
 
     List<String> imgList = new ArrayList<String>();
 
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+
 
 
     //Click on shit to close it...
@@ -25,9 +35,21 @@ public class MainActivity extends AppCompatActivity {
     final ImageView image3 = (ImageView) findViewById(R.id.imageZeus);
     final ImageView image4 = (ImageView) findViewById(R.id.imageHome);
     final ImageView image5 = (ImageView) findViewById(R.id.imageWinner);
+    final ImageView image6 = (ImageView) findViewById(R.id.imageBudLight);
+    final ImageView image7 = (ImageView) findViewById(R.id.imageSingle);
 
 
 
+        // hide status and navigation bar
+    View mDecorView = getWindow().getDecorView();
+
+    mDecorView.setSystemUiVisibility(
+            View.SYSTEM_UI_FLAG_LAYOUT_STABLE
+                    | View.SYSTEM_UI_FLAG_LAYOUT_HIDE_NAVIGATION
+                    | View.SYSTEM_UI_FLAG_LAYOUT_FULLSCREEN
+                    | View.SYSTEM_UI_FLAG_HIDE_NAVIGATION   // hide nav bar
+                    | View.SYSTEM_UI_FLAG_FULLSCREEN        // hide status bar
+                    | View.SYSTEM_UI_FLAG_IMMERSIVE);
 
     image1.setOnClickListener( new View.OnClickListener()
     {
@@ -36,11 +58,35 @@ public class MainActivity extends AppCompatActivity {
             image1.setVisibility(View.GONE);
 
             imgList.add("img0");
-
+/*
             if(!(imgList.indexOf("img0") == 0)) {
                 Intent myIntent = new Intent(MainActivity.this, TimeWaste.class);
                 MainActivity.this.startActivity(myIntent);
 
+            }*/
+            if (imgList.size() != 7) {
+                //
+            }
+            else {
+                if (!(imgList.indexOf("img6") == 6) && (imgList.indexOf("img5") == 5) && (imgList.indexOf("img4") == 4) && (imgList.indexOf("img3") == 3) && (imgList.indexOf("img2") == 2) && (imgList.indexOf("img1") == 1) && (imgList.indexOf("img0") == 0)) {
+
+                    System.out.println("Hello");
+                    android.os.Process.killProcess(android.os.Process.myPid());
+                    System.exit(1);
+                    System.out.println("SUCCESS");
+                } else {
+                    System.out.println("SUCCESSFUL-NOT");
+
+                    // launch success activity OR kill application
+                    //Intent myIntent = new Intent(MainActivity.this, SuccessWindow.class);
+                    //MainActivity.this.startActivity(myIntent);
+                    System.out.println("1");
+                    imgList.clear();
+                    Intent myIntent = new Intent(MainActivity.this, TimeWaste.class);
+                    //MainActivity.this.startActivity(myIntent);
+                    startActivity(myIntent);
+                    finish();
+                }
             }
         }
     });
@@ -52,10 +98,31 @@ public class MainActivity extends AppCompatActivity {
             image2.setVisibility(View.GONE);
 
             imgList.add("img1");
-
+/*
             if(!(imgList.indexOf("img1") == 1)) {
                 Intent myIntent = new Intent(MainActivity.this, TimeWaste.class);
                 MainActivity.this.startActivity(myIntent);
+            }*/
+            if (imgList.size() != 7) {
+                //
+            }
+            else {
+                if (!(imgList.indexOf("img6") == 6) && (imgList.indexOf("img5") == 5) && (imgList.indexOf("img4") == 4) && (imgList.indexOf("img3") == 3) && (imgList.indexOf("img2") == 2) && (imgList.indexOf("img1") == 1) && (imgList.indexOf("img0") == 0)) {
+
+                    System.out.println("Hello");
+                    android.os.Process.killProcess(android.os.Process.myPid());
+                    System.exit(1);
+                } else {
+                    // launch success activity OR kill application
+                    //Intent myIntent = new Intent(MainActivity.this, SuccessWindow.class);
+                    //MainActivity.this.startActivity(myIntent);
+                    System.out.println("2");
+                    imgList.clear();
+                    Intent myIntent = new Intent(MainActivity.this, TimeWaste.class);
+                    //MainActivity.this.startActivity(myIntent);
+                    startActivity(myIntent);
+                    finish();
+                }
             }
         }
     });
@@ -67,10 +134,31 @@ public class MainActivity extends AppCompatActivity {
             image3.setVisibility(View.GONE);
 
             imgList.add("img2");
-
+/*
             if(!(imgList.indexOf("img2") == 2)) {
                 Intent myIntent = new Intent(MainActivity.this, TimeWaste.class);
                 MainActivity.this.startActivity(myIntent);
+            }*/
+            if (imgList.size() != 7) {
+                //
+            }
+            else {
+                if (!(imgList.indexOf("img6") == 6) && (imgList.indexOf("img5") == 5) && (imgList.indexOf("img4") == 4) && (imgList.indexOf("img3") == 3) && (imgList.indexOf("img2") == 2) && (imgList.indexOf("img1") == 1) && (imgList.indexOf("img0") == 0)) {
+
+                    System.out.println("Hello");
+                    android.os.Process.killProcess(android.os.Process.myPid());
+                    System.exit(1);
+                } else {
+                    // launch success activity OR kill application
+                    //Intent myIntent = new Intent(MainActivity.this, SuccessWindow.class);
+                    //MainActivity.this.startActivity(myIntent);
+                    System.out.println("3");
+                    imgList.clear();
+                    Intent myIntent = new Intent(MainActivity.this, TimeWaste.class);
+                    //MainActivity.this.startActivity(myIntent);
+                    startActivity(myIntent);
+                    finish();
+                }
             }
         }
     });
@@ -82,10 +170,31 @@ public class MainActivity extends AppCompatActivity {
             image4.setVisibility(View.GONE);
 
             imgList.add("img3");
-
+/*
             if(!(imgList.indexOf("img3") == 3)) {
                 Intent myIntent = new Intent(MainActivity.this, TimeWaste.class);
                 MainActivity.this.startActivity(myIntent);
+            }*/
+            if (imgList.size() != 7) {
+                //
+            }
+            else {
+                if (!(imgList.indexOf("img6") == 6) && (imgList.indexOf("img5") == 5) && (imgList.indexOf("img4") == 4) && (imgList.indexOf("img3") == 3) && (imgList.indexOf("img2") == 2) && (imgList.indexOf("img1") == 1) && (imgList.indexOf("img0") == 0)) {
+
+                    System.out.println("Hello");
+                    android.os.Process.killProcess(android.os.Process.myPid());
+                    System.exit(1);
+                } else {
+                    // launch success activity OR kill application
+                    //Intent myIntent = new Intent(MainActivity.this, SuccessWindow.class);
+                    //MainActivity.this.startActivity(myIntent);
+                    System.out.println("4");
+                    imgList.clear();
+                    Intent myIntent = new Intent(MainActivity.this, TimeWaste.class);
+                    //MainActivity.this.startActivity(myIntent);
+                    startActivity(myIntent);
+                    finish();
+                }
             }
 
         }
@@ -98,19 +207,124 @@ public class MainActivity extends AppCompatActivity {
             image5.setVisibility(View.GONE);
 
             imgList.add("img4");
-
+/*
             if(!(imgList.indexOf("img4") == 4)) {
                 Intent myIntent = new Intent(MainActivity.this, TimeWaste.class);
                 MainActivity.this.startActivity(myIntent);
+            }*/
+            if (imgList.size() != 7) {
+                //
             }
             else {
-                // launch success activity
-                Intent myIntent = new Intent(MainActivity.this, SuccessWindow.class);
-                MainActivity.this.startActivity(myIntent);
+                if (!(imgList.indexOf("img6") == 6) && (imgList.indexOf("img5") == 5) && (imgList.indexOf("img4") == 4) && (imgList.indexOf("img3") == 3) && (imgList.indexOf("img2") == 2) && (imgList.indexOf("img1") == 1) && (imgList.indexOf("img0") == 0)) {
+
+                    System.out.println("Hello");
+                    android.os.Process.killProcess(android.os.Process.myPid());
+                    System.exit(1);
+                } else {
+                    // launch success activity OR kill application
+                    //Intent myIntent = new Intent(MainActivity.this, SuccessWindow.class);
+                    //MainActivity.this.startActivity(myIntent);
+                    System.out.println("5");
+                    imgList.clear();
+                    Intent myIntent = new Intent(MainActivity.this, TimeWaste.class);
+                    //MainActivity.this.startActivity(myIntent);
+                    startActivity(myIntent);
+                    finish();
+                }
             }
         }
     });
 
+
+    image6.setOnClickListener( new View.OnClickListener()
+        {
+            @Override
+            public void onClick(View v) {
+                image6.setVisibility(View.GONE);
+
+                imgList.add("img5");
+/*
+                if(!(imgList.indexOf("img5") == 5)) {
+                    Intent myIntent = new Intent(MainActivity.this, TimeWaste.class);
+                    MainActivity.this.startActivity(myIntent);
+                }*/
+                if (imgList.size() != 7) {
+                    //
+                }
+                else {
+                    if (!(imgList.indexOf("img6") == 6) && (imgList.indexOf("img5") == 5) && (imgList.indexOf("img4") == 4) && (imgList.indexOf("img3") == 3) && (imgList.indexOf("img2") == 2) && (imgList.indexOf("img1") == 1) && (imgList.indexOf("img0") == 0)) {
+
+                        System.out.println("Hello");
+
+                        android.os.Process.killProcess(android.os.Process.myPid());
+                        System.exit(1);
+                    } else {
+                        // launch success activity OR kill application
+                        //Intent myIntent = new Intent(MainActivity.this, SuccessWindow.class);
+                        //MainActivity.this.startActivity(myIntent);
+                        System.out.println("6");
+                        imgList.clear();
+                        Intent myIntent = new Intent(MainActivity.this, TimeWaste.class);
+                        //MainActivity.this.startActivity(myIntent);
+                        startActivity(myIntent);
+                        finish();
+                    }
+                }
+            }
+        });
+
+    image7.setOnClickListener( new View.OnClickListener()
+        {
+            @Override
+            public void onClick(View v) {
+                image7.setVisibility(View.GONE);
+
+                imgList.add("img6");
+                if (imgList.size() != 7) {
+                    // do nothing
+                }
+                else {
+                        if ((imgList.indexOf("img6") == 6) && (imgList.indexOf("img5") == 5) && (imgList.indexOf("img4") == 4) && (imgList.indexOf("img3") == 3) && (imgList.indexOf("img2") == 2) && (imgList.indexOf("img1") == 1) && (imgList.indexOf("img0") == 0)) {
+
+                            getWindow().getDecorView().setBackgroundColor(Color.rgb(45, 134, 48));
+
+                            TextView successText = (TextView)findViewById(R.id.textView5);
+                            successText.setText("Success");
+
+                            System.out.println("FINAL STAGE");
+
+                            MediaPlayer ring = MediaPlayer.create(MainActivity.this,R.raw.rick);
+                            ring.start();
+
+
+                            Handler handler = new Handler();
+                            handler.postDelayed(new Runnable() {
+                                @Override
+                                public void run() {
+                                    System.out.println("Who's your daddy?");
+                                    android.os.Process.killProcess(android.os.Process.myPid());
+                                    System.exit(1);
+                                }
+                            }, 3000);
+
+
+
+                        } else {
+                            // launch success activity OR kill application
+                            //Intent myIntent = new Intent(MainActivity.this, SuccessWindow.class);
+                            //MainActivity.this.startActivity(myIntent);
+
+                            System.out.println("7");
+                            imgList.clear();
+                            Intent myIntent = new Intent(MainActivity.this, TimeWaste.class);
+                            //MainActivity.this.startActivity(myIntent);
+                            startActivity(myIntent);
+                            finish();
+                    }
+                }
+            }
+        });
 
 
     }
--- MyApplication/app/src/main/java/com/example/mverw/myapplication/TimeWaste.java
+++ MyApplication/app/src/main/java/com/example/mverw/myapplication/TimeWaste.java
@@ -7,6 +7,8 @@ import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.widget.VideoView;
 
+import android.view.View;
+
 public class TimeWaste extends AppCompatActivity {
 
 
@@ -18,7 +20,7 @@ public class TimeWaste extends AppCompatActivity {
 
         VideoView mVideoView2 = (VideoView)findViewById(R.id.videoView);
 
-        String uriPath2 = "android.resource://com.example.mverw.myapplication/"+R.raw.taidaddy;
+        String uriPath2 = "android.resource://com.example.mverw.myapplication/"+R.raw.rick;
         Uri uri2 = Uri.parse(uriPath2);
         mVideoView2.setVideoURI(uri2);
         mVideoView2.requestFocus();
@@ -26,11 +28,21 @@ public class TimeWaste extends AppCompatActivity {
 
         mVideoView2.setOnCompletionListener(new MediaPlayer.OnCompletionListener() {
             public void onCompletion(MediaPlayer mp) {
-                // u suffer enough
+                // you suffer enough
                 Intent myIntent = new Intent(TimeWaste.this, MainActivity.class);
                 TimeWaste.this.startActivity(myIntent);
             }
         });
+
+        View mDecorView = getWindow().getDecorView();
+
+        mDecorView.setSystemUiVisibility(
+                View.SYSTEM_UI_FLAG_LAYOUT_STABLE
+                        | View.SYSTEM_UI_FLAG_LAYOUT_HIDE_NAVIGATION
+                        | View.SYSTEM_UI_FLAG_LAYOUT_FULLSCREEN
+                        | View.SYSTEM_UI_FLAG_HIDE_NAVIGATION   // hide nav bar
+                        | View.SYSTEM_UI_FLAG_FULLSCREEN        // hide status bar
+                        | View.SYSTEM_UI_FLAG_IMMERSIVE);
 
 
     }
